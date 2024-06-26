6815cf2d749c96684b28eccd73f7e30963e2eb73
etienneDev <etienne.baillif97436@gmail.com>
2024-06-26 18:40:23 +0200

    reset btn feature

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Title } from '@angular/platform-browser'; 
+import { Title } from '@angular/platform-browser';
 import { Projet } from '../modele/Projet';
 import { Tag } from '../modele/tag';
 import { ProjectsService } from '../_services/projects.service';
@@ -7,27 +7,50 @@ import { ProjectsService } from '../_services/projects.service';
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.css']
+  styleUrls: ['./portfolio.component.css'],
 })
-export class PortfolioComponent implements OnInit{
-  
+export class PortfolioComponent implements OnInit {
   lesProjets = {} as Projet[];
 
   isCollapsed: boolean = true;
-  java: boolean = false;
+  filtering: boolean = false;
 
-  constructor(private titleService: Title, private projectService : ProjectsService) {
-    this.titleService.setTitle("Etienne Baillif - PortFolio");
+  //language
+  java: boolean = false;
+  springBoot: boolean = false;
+
+  constructor(
+    private titleService: Title,
+    private projectService: ProjectsService
+  ) {
+    this.titleService.setTitle('Etienne Baillif - PortFolio');
   }
   ngOnInit(): void {
     this.lesProjets = this.projectService.GetProjects();
   }
 
-  Filter(){
+  Filter() {
     let filterTags: Tag[] = [];
-    if(this.java){
+    if (this.java) {
       filterTags.push(Tag.JAVA);
     }
+    if (this.springBoot) {
+      filterTags.push(Tag.SPRING);
+    }
+
+    //etat du filtre
+    if (filterTags.length > 0) {
+      this.filtering = true;
+    } else {
+      this.filtering = false;
+    }
     this.lesProjets = this.projectService.GetProjectByFilter(filterTags);
+  }
+
+  ResetFilter() {
+    this.java = false;
+    this.springBoot = false;
+    this.filtering = false;
+    this.lesProjets = this.projectService.GetProjects();
   }
 }
