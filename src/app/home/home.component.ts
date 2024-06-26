14b1764c4d1e7d0c691ccd5b977b7dde80cb1047
etienneDev <etienne.baillif97436@gmail.com>
2024-06-26 14:39:01 +0200

    data services

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { ProjectsService } from '../_services/projects.service';
+import { Projet } from '../modele/Projet';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-  constructor(private titleService: Title) {
+export class HomeComponent implements OnInit {
+  projetActuel = {} as Projet;
+  constructor(private titleService: Title, private projectService: ProjectsService) {
     this.titleService.setTitle("Etienne Baillif - Accueil");
+  }
+  ngOnInit(): void {
+   this.projetActuel = this.projectService.GetProjectById(1);
   }
 }
--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,25 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser'; 
 import { Projet } from '../modele/Projet';
 import { Tag } from '../modele/tag';
+import { ProjectsService } from '../_services/projects.service';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.css']
 })
-export class PortfolioComponent {
-  unprojet: Projet = {
-    id:0,
-    nom:'Plante EEE',
-    resume:'BLABLABLA',
-    description:'LALALALLA',
-    projectLink:'https//xxx',
-    photo: [],
-    tags: [Tag.ANGULAR, Tag.JAVA, Tag.JavaScript, Tag.SPRING],
-  }
-  constructor(private titleService: Title) {
-    this.titleService.setTitle("Etienne Baillif - PortFolio");
+export class PortfolioComponent implements OnInit{
+  
+  lesProjets = {} as Projet[];
 
+  constructor(private titleService: Title, private projectService : ProjectsService) {
+    this.titleService.setTitle("Etienne Baillif - PortFolio");
+  }
+  ngOnInit(): void {
+    this.lesProjets = this.projectService.GetProjects();
   }
 }
